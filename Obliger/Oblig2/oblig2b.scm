c0f2ffd6a3aacafac5ccd66f2978040fc36e288a
Stian Kongsvik <stianko@gmail.com>
2015-03-24 16:37:52 +0100

    dsfjkndskjfn

--- Obliger/Oblig2/oblig2b.scm
+++ Obliger/Oblig2/oblig2b.scm
@@ -1,4 +1,7 @@
 (require r5rs)
+(print-as-expression #f)
+(print-mpair-curly-braces #f)
+
 ;; 
 ;; Oblig 2b, INF2810 v15
 ;;
@@ -11,7 +14,38 @@
       (begin (set! count (+ count 1))
 	     count))))
 ;;
+;; (b)
 ;; -------------------------------------------------------------
 ;; -----------------------Fancy diagram-------------------------
 ;; -------------------------------------------------------------
 
+;; Oppg. 2
+;; (a)
+
+(define (make-stack stack)
+  (define (push! list)
+    (set! stack (append (reverse list) stack)))
+  (define (pop!)
+    (if (not (null? stack))
+    (set! stack (cdr stack))))
+  (define (pass-message message . r)
+      (cond ((eq? message 'push!) (push! r))
+	    ((eq? message 'pop!) (pop!))
+	    ((eq? message 'stack) stack)))
+  pass-message)
+
+;; (b)
+
+(define (push! stack-object . r)
+  (if (null? r)
+      '()
+      (stack-object 'push! (car r)))
+  (display r))
+      ;;(push! stack-object (cdr r))))
+
+(define (pop! stack)
+  (stack 'pop!))
+
+(define (stack stack-object)
+  (stack-object 'stack))
+  
